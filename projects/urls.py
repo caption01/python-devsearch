4dae628272a1e64e59bc06ba9d80307791135687
caption01 <nutchapon.han@gmail.com>
2021-07-27 14:56:35 +0700

    add form template

--- projects/urls.py
+++ projects/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.projects, name='projects'),
-    path('project/<str:pk>', views.project, name='project')
+    path('project/<str:pk>', views.project, name='project'),
+
+    path('create-project/', views.create_project, name='create-project')
 ]
--- projects/views.py
+++ projects/views.py
@@ -1,25 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Project
+from .forms import ProjectForm
 
-projectsList = [
-    {
-        'id': '1',
-        'title': 'Ecommerce Website',
-        'description': 'Fully functional ecommerce website'
-    },
-    {
-        'id': '2',
-        'title': 'Portfolio Website',
-        'description': 'A personal website to write articles and display work'
-    },
-    {
-        'id': '3',
-        'title': 'Social Network',
-        'description': 'An open source project built by the community'
-    }
-]
-# Create your views here.
 def projects(request):
     projects = Project.objects.all()
     context = { 'projects': projects }
@@ -31,3 +14,10 @@ def project(request, pk):
         'project': projectObj,
     }
     return render(request, 'projects/single-project.html', context)
+
+def create_project(request):
+    form = ProjectForm()
+    context = {
+        'form': form
+    }
+    return render(request, 'projects/project_form.html', context)
