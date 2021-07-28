a1fbe808369fc7cdad2f3830536a1580010d5d53
caption01 <nutchapon.han@gmail.com>
2021-07-28 20:12:06 +0700

    add account view

--- users/views.py
+++ users/views.py
@@ -88,3 +88,16 @@ def register_user(request):
         'form': form,
     }
     return render(request, 'users/login-register.html', context)
+
+login_required(login_url='login')
+def user_account(request):
+    profile = request.user.profile
+    skills = profile.skill_set.all()
+    projects = profile.project_set.all()
+
+    context = {
+        'profile': profile,
+        'skills': skills,
+        'projects': projects
+    }
+    return render(request, 'users/account.html', context)
